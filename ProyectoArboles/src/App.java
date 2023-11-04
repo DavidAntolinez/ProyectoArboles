65e77e21f9bd6fde645cec770f59de1e81179c81
DavidAntolinez <128693961+DavidAntolinez@users.noreply.github.com>
2023-11-04 14:48:13 -0500

    Add files via upload

---
+++ ProyectoArboles/src/App.java
@@ -0,0 +1,12 @@
+
+
+import logica.Larbol;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Larbol h = new Larbol("GBHADCFEMJNILK");
+        
+        h.MostratArbol();
+        
+    }
+}
---
+++ ProyectoArboles/src/logica/Larbol.java
@@ -0,0 +1,328 @@
+package logica;
+
+import java.util.ArrayList;
+
+import bean.Barbol;
+import bean.Nodos;
+
+public class Larbol extends Barbol {
+
+    public Larbol(String cadena) {
+        char[] datos = cadena.toCharArray();
+        for (int i = 0; i < datos.length; i++) {
+            Insertar(datos[i], getPunta());
+        }
+    }
+
+    public void RecorrerInorden(Nodos R) {
+        if (R != null) {
+            RecorrerInorden(R.getLi());
+            Mostrar(R.getDato());
+            RecorrerInorden(R.getLd());
+        }
+    }
+
+    public void Mostrar(char a) {
+        System.out.print(a);
+    }
+
+    public void RecorrerPosorden(Nodos R) {
+        if (R != null) {
+            RecorrerPosorden(R.getLi());
+            RecorrerPosorden(R.getLd());
+            Mostrar(R.getDato());
+        }
+    }
+
+    public void RecorrerPreorden(Nodos R) {
+        if (R != null) {
+            Mostrar(R.getDato());
+            RecorrerPreorden(R.getLi());
+            RecorrerPreorden(R.getLd());
+        }
+    }
+
+    public void h() {
+        System.out.println("null");
+    }
+
+    public void Insertar(char dato, Nodos P) {
+        if (getPunta() == null) {
+            setPunta(new Nodos(dato));
+        } else {
+            if (dato > P.getDato()) {
+                if (P.getLd() == null) {
+                    P.setLd(new Nodos(dato));
+                } else {
+                    Insertar(dato, P.getLd());
+                }
+            } else if (dato < P.getDato()) {
+                if (P.getLi() == null) {
+                    P.setLi(new Nodos(dato));
+                } else {
+                    Insertar(dato, P.getLi());
+                }
+            }
+        }
+    }
+
+    public void Mostrar1hijos(Nodos R) {
+        if (R != null) {
+            Mostrar1hijos(R.getLi());
+            if ((R.getLd() == null && R.getLi() != null) || (R.getLd() != null && R.getLi() == null)) {
+                Mostrar(R.getDato());
+            }
+            Mostrar1hijos(R.getLd());
+        }
+    }
+
+    public void MostrarPadres(Nodos R, char dato, String cadena) {
+        if (R != null) {
+            if (R.getDato() > dato) {
+                cadena += R.getDato();
+                MostrarPadres(R.getLi(), dato, cadena);
+            } else if (R.getDato() < dato) {
+                cadena += R.getDato();
+                MostrarPadres(R.getLd(), dato, cadena);
+            } else {
+                StringBuilder stringbuilder = new StringBuilder(cadena);
+                String invertida = stringbuilder.reverse().toString();
+                System.out.println(invertida);
+            }
+        }
+    }
+
+    public void MostrarHojas(Nodos R) {
+        if (R != null) {
+            MostrarHojas(R.getLi());
+            if (R.getLd() == null & R.getLi() == null) {
+                Mostrar(R.getDato());
+            }
+            MostrarHojas(R.getLd());
+        }
+    }
+
+    public void MostrarLi(Nodos R) {
+        if (R != null) {
+            MostrarLi(R.getLi());
+            if (R.getLd() == null & R.getLi() != null) {
+                Mostrar(R.getDato());
+            }
+            MostrarLi(R.getLd());
+        }
+    }
+
+    public boolean MostrarHermanoDerecho(Nodos R, char dato, boolean bandera) {
+        if (R != null) {
+            if (R.getLi() != null & R.getLd() != null) {
+                if (R.getLi().getDato() == dato) {
+                    System.out.print("El dato tiene un hermano derecho, el cual es: ");
+                    Mostrar(R.getLd().getDato());
+                    System.out.println();
+                    bandera = true;
+                }
+            }
+            if (!bandera & R.getDato() > dato) {
+                bandera = MostrarHermanoDerecho(R.getLi(), dato, bandera);
+            } else if (R.getDato() < dato) {
+                bandera = MostrarHermanoDerecho(R.getLd(), dato, bandera);
+            }
+        }
+        return bandera;
+    }
+
+    public boolean MostrarHermanoIzquierdo(Nodos R, char dato, boolean bandera) {
+        if (R != null) {
+            if (R.getLi() != null & R.getLd() != null) {
+                if (R.getLd().getDato() == dato) {
+                    System.out.print("El dato tiene un hermano izquierdo, el cual es: ");
+                    Mostrar(R.getLi().getDato());
+                    System.out.println();
+                    bandera = true;
+                }
+            }
+            if (!bandera & R.getDato() > dato) {
+                bandera = MostrarHermanoIzquierdo(R.getLi(), dato, bandera);
+            } else if (R.getDato() < dato) {
+                bandera = MostrarHermanoIzquierdo(R.getLd(), dato, bandera);
+            }
+        }
+
+        return bandera;
+    }
+
+    public void HermanoDelDato(Nodos R, char dato, boolean bandera) {
+        if (MostrarHermanoDerecho(R, dato, false)) {
+        } else if (MostrarHermanoIzquierdo(R, dato, false)) {
+        } else {
+            System.out.println("El dato no tiene hermanos");
+        }
+    }
+
+    public int ContarHojas(Nodos R, int cont) {
+        if (R != null) {
+            cont = ContarHojas(R.getLi(), cont);
+            cont = ContarHojas(R.getLd(), cont);
+            if (R.getLd() == null & R.getLi() == null) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
+    public int ContarPadres(Nodos R, int cont) {
+        if (R != null) {
+            cont = ContarPadres(R.getLi(), cont);
+            cont = ContarPadres(R.getLd(), cont);
+            if (R.getLd() != null | R.getLi() != null) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
+    public void Altura(char dato) {
+        Nodos R = Buscardato(getPunta(), dato);
+        System.out.println("La altura de " + dato + " es: " + CalcularAltura(R, 0));
+    }
+
+    public int CalcularAltura(Nodos R, int cont) {
+        if (R != null) {
+            int cont2 = 0;
+            int cont3 = 0;
+            if (R.getLi() != null) {
+                cont2 = CalcularAltura(R.getLi(), cont + 1);
+            }
+            if (R.getLd() != null) {
+                cont3 = CalcularAltura(R.getLd(), cont + 1);
+            }
+            if (R.getLi() != null || R.getLd() != null) {
+                if (cont2 > cont3) {
+                    cont = cont2;
+                } else {
+                    cont = cont3;
+                }
+            }
+        }
+        return cont;
+    }
+
+    public Nodos Buscardato(Nodos R, char dato) {
+        if (R != null) {
+            if (R.getDato() > dato) {
+                R = Buscardato(R.getLi(), dato);
+            } else if (R.getDato() < dato) {
+                R = Buscardato(R.getLd(), dato);
+            } else {
+                return R;
+            }
+        }
+        return R;
+    }
+
+    public void Ancestros(Nodos R, char dato) {
+        if (R != null) {
+            if (R.getDato() > dato) {
+                Ancestros(R.getLi(), dato);
+                System.out.print(R.getDato());
+            } else if (R.getDato() < dato) {
+                Ancestros(R.getLd(), dato);
+                System.out.print(R.getDato());
+            }
+        }
+    }
+
+    public void NivelDato(Nodos R, char dato, int cont) {
+        if (R != null) {
+            if (R.getDato() > dato) {
+                NivelDato(R.getLi(), dato, cont + 1);
+            } else if (R.getDato() < dato) {
+                NivelDato(R.getLd(), dato, cont + 1);
+            } else {
+                System.out.println("El nivel del dato es: " + cont);
+            }
+        }
+    }
+
+    public int CalcularPrimos(Nodos R, char dato, int cont) {
+        if (R != null) {
+            if (cont == 0) {
+                if (R.getDato() > dato) {
+                    cont = CalcularPrimos(R.getLi(), dato, cont);
+                } else if (R.getDato() < dato) {
+                    cont = CalcularPrimos(R.getLd(), dato, cont);
+                } else {
+                    return 1;
+                }
+            }
+            if (cont == 1) {
+                return 2;
+            } else if (cont == 2) {
+                if (R.getDato() > dato) {
+                    if (R.getLd() != null) {
+                        cont = CalcularPrimos(R.getLd(), dato, cont + 1);
+                    } else {
+                        return 5;
+                    }
+                } else if (R.getDato() < dato) {
+                    if (R.getLi() != null) {
+                        cont = CalcularPrimos(R.getLi(), dato, cont + 1);
+                    } else {
+                        return 5;
+                    }
+                }
+            }
+            if (cont == 3) {
+                cont++;
+                if (R.getLi() == null && R.getLd() == null) {
+                    return 5;
+                } else {
+                    if (R.getLd() != null) {
+                        System.out.print(R.getLd().getDato());
+                    }
+                    if (R.getLi() != null) {
+                        System.out.print(R.getLi().getDato());
+                    }
+                }
+            }
+        }
+        return cont;
+    }
+
+    public void Primos(char dato) {
+        int cont = CalcularPrimos(getPunta(), dato, 0);
+        if (cont != 4) {
+            System.out.println("El dato no tiene primos");
+        }
+        System.out.println();
+    }
+
+    public void MostratArbol() {
+        ArrayList<String[]> lista = new ArrayList<String[]>();
+        MostrarArbolR(getPunta(), lista, 0);
+        int Nmayor = 0;
+        for (String[] strings : lista) {
+            if (Integer.parseInt(strings[1]) > Nmayor) {
+                Nmayor = Integer.parseInt(strings[1]);
+            }
+        }
+        
+        for (int i = 0; i <= Nmayor; i++) {
+            for (String[] strings : lista) {
+                if (Integer.parseInt(strings[1]) == i) {
+                    System.out.print(strings[0]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private void MostrarArbolR(Nodos R, ArrayList<String[]> lista, int cont) {
+        if (R != null) {
+            String[] elemento = { R.getDato() + "", cont + "" ,""};
+            lista.add(elemento);
+            MostrarArbolR(R.getLi(), lista, cont + 1);
+            MostrarArbolR(R.getLd(), lista, cont + 1);
+        }
+    }
+}
